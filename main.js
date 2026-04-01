4a9d10ec92d804e087bc2ce2117a288fd0cbe5e6
TimofeyMeshcheryakov <tdmesherykov@mail.ru>
2026-04-01 08:39:44 +0300

    start 2.0

---
+++ main.js
@@ -0,0 +1,72 @@
+console.log(document);
+
+const title = document.getElementById("title");
+const text = document.querySelector(".text");
+
+console.log(title);
+console.log(text);
+
+title.textContent = "DOM изменён через JS!";
+title.style.color = "blue";
+title.style.backgroundColor = "#f0f0f0";
+title.style.padding = "10px";
+
+title.style.fontSize = "18px";
+text.style.fontSize = "italic";
+text.style.color = "darkgreen";
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    alert("Кнопка нажата!");
+    btn.style.backgroundColor = "#4CAF50";
+    btn.style.color = "white";
+});
+
+const input = document.getElementById("nameInput");
+const button = document.getElementById("showName");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+    if (input.value.trim() !== "") {
+        output.textContent = `Привет, ${input.value}!`;
+        output.style.color = "green";
+    } else {
+        output.textContent = "Пожалуйста, введите имя!";
+        output.style.color = "red";
+    }
+});
+
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", () => {
+    input.value = "";
+    output.textContent = "";
+    input.focus();
+});
+
+const form = document.getElementById("userForm");
+const usernameInput = document.getElementById("username");
+const ageInput = document.getElementById("age");
+const result = document.getElementById("formResult");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = usernameInput.value.trim();
+    const age = Number(ageInput.value);
+    if (name === "") {
+        result.textContent = `Ошибка: Имя не может быть пустым!`;
+        result.style.color = `red`;
+        usernameInput.focus();
+        return;
+    }
+    if (isNaN(age) || age <= 0 || age > 120) {
+        result.textContent = `Ошибка: Введите корректный возраст (от 1 до 120)!`;
+        result.style.color = "red";
+        ageInput.focus();
+        ageInput.value = "";
+        return;
+    }
+    result.textContent = `Данные сохранены: ${name}, ${age} лет`
+    result.style.color = "green"
+        
+});
